3abe301b1d77f424e64ceda40ba8f09a63b630d3
xielei <xielei@creai.cn>
2019-10-24 13:35:05 +0800

    '支持标签和名称管理'

--- myblog/models.py
+++ myblog/models.py
@@ -63,7 +63,6 @@ class Admin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), unique=True, nullable=False)
-    about_me = db.Column(db.Text)
 
     @property
     def password(self):

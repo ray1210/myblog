2521b74044b8d788d07d9b2d4d21c8436aaf0b08
xielei <xielei@creai.cn>
2019-10-28 17:52:21 +0800

    update mistune

--- myblog/plugins.py
+++ myblog/plugins.py
@@ -35,7 +35,8 @@ login_manager.login_message_category = 'warning'
 class HighlightRenderer(mistune.Renderer):
     def block_code(self, code, lang):
         if not lang:
-            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
+            lang = 'bash'
+            #return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
         try:
             lexer = get_lexer_by_name(lang, stripall=True)
         except ClassNotFound as e:
